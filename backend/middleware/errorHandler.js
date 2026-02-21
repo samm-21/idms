9d50e6ba459e08d52d0364a9b9b2995c966883b7
Sam <144946983+samm-21@users.noreply.github.com>
2026-02-21 15:47:02 +0530

    Delete backend directory

--- backend/middleware/errorHandler.js
+++
@@ -1,7 +0,0 @@
-module.exports = (err, req, res, next) => {
-  console.error(err.message);
-  res.status(500).json({
-    success: false,
-    message: err.message || "Server Error"
-  });
-};
--- backend/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const { register, login, logout } = require("../controllers/auth");
-
-router.post("/register", register); // use once
-router.post("/login", login);
-router.post("/logout", logout);
-
-module.exports = router;
